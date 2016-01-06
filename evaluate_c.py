e222c750f5143686f96514aa41d4c55dfca7368a
csinva <csinva@yahoo.com>
2016-01-06 11:01:36 -0500

    plots correct data

--- evaluate_c.py
+++ evaluate_c.py
@@ -2,37 +2,38 @@ import numpy as np
 import sys
 import array
 import time
+import os
 import h5py
 sys.path.append('src_cython')
 from mainDefs import eval
 
 start = time.clock()
-dims = np.array([72,936,936,11],dtype='uint32')
+dims = np.array([256,256,256,3],dtype='uint32')
 
+########################### RAND ERROR ######################################
 # gt
-f = open("/groups/turaga/turagalab/greentea_experiments/project_data/labels_id_cropped.raw")
+f = open("/groups/turaga/home/singhc/evaluation/data/gt.in")
 a = array.array("I")
 a.fromfile(f, dims[0]*dims[1]*dims[2])
 print(a[0:10])
 f.close()
 
 # affs
-f = open("/groups/turaga/home/turagas/research/caffe_neural_models/dataset_07/processed/train_euclid.raw")
+f = open("/groups/turaga/home/singhc/evaluation/data/ws_test_256.raw")
 aff = array.array("f")
 aff.fromfile(f, dims[0]*dims[1]*dims[2]*dims[3])
 print(aff[0:10])
 f.close()
 
 # threshes
-# threshes = [100+i*100 for i in range(0,10)]+[i*1000 for i in range(2,11)]+[i*10000 for i in range(2,11)] # 100...1,000...100,000
-threshes = [100+i*300 for i in range(0,4)]+[i*3000 for i in range(1,4)]+[i*30000 for i in range(2,4)] # 100...1,000...100,000
+threshes = [100+i*100 for i in range(0,10)]+[i*1000 for i in range(2,11)]+[i*10000 for i in range(2,11)] # 100...1,000...100,000
+# threshes = [100+i*300 for i in range(0,4)]+[i*3000 for i in range(1,4)]+[i*30000 for i in range(2,4)] # 100...1,000...100,000
+# threshes = [i*10 for i in range(0,21)]+ [i*25000 for i in range(1,11)]
 print threshes
 
 # output folder
-out = 'test_arb_fast/'
+out = 'out/test_full/'
 
 gt = np.frombuffer(a,dtype='uint32').reshape(dims[0:3])
 affs = np.frombuffer(aff,dtype='float32').reshape(dims)
 eval(gt,affs,threshes,out)
-end = time.clock()
-print (end-start)*60,"seconds elapsed"
--- plot_results.py
+++ plot_results.py
@@ -3,32 +3,38 @@ import pylab
 import sys
 import os.path as op
 
-target_folder = "out"
+target_folder = "test_full"
 
-a=np.fromfile( op.join(target_folder, 'linear.dat') )
+'''
+print len(sys.argv)
+for arg in sys.argv:
+    print arg
+'''
+
+outfolder = 'out/'+target_folder
+a=np.fromfile( op.join(outfolder , 'linear.dat') )
 print a
 linear=a.reshape(len(a)/2,2)
 pylab.plot( linear[:,0], linear[:,1], 'ro-', label='linear')
 pylab.hold(True)
 
-a=np.fromfile( op.join(target_folder, 'square.dat') )
+a=np.fromfile( op.join(outfolder, 'square.dat') )
 linear=a.reshape(len(a)/2,2)
 pylab.plot( linear[:,0], linear[:,1], 'bo-', label='square')
 
-a=np.fromfile( op.join(target_folder, 'felzenszwalb.dat') )
+a=np.fromfile( op.join(outfolder, 'felzenszwalb.dat') )
 linear=a.reshape(len(a)/2,2)
 pylab.plot( linear[:,0], linear[:,1], 'ko-', label='felzenszwalb')
 
-'''
-a=np.fromfile( op.join(target_folder, 'threshold.dat') )
+
+a=np.fromfile( op.join(outfolder, 'threshold.dat') )
 linear=a.reshape(len(a)/2,2)
 pylab.plot( linear[:,0], linear[:,1], 'go-', label='threshold')
 
 
-'''
-
 pylab.xlabel('Rand Split')
 pylab.ylabel('Rand Merge')
-
-pylab.legend()	
+pylab.legend()
+pylab.savefig('figs/'+target_folder+'_results.png')
 pylab.show()
+
