af7a42dfdc63e90d911548a5b5800cca2e320260
csinva <csinva@yahoo.com>
2016-01-06 13:48:02 -0500

    evaluate in parallel

--- plot_results.py
+++ plot_results.py
@@ -3,38 +3,45 @@ import pylab as plt
 import sys
 import os.path as op
 
+def formatAndSave(ax):
+    plt.xlabel('Rand Split')
+    plt.ylabel('Rand Merge')
+    #plt.xlim([0,1])
+    #plt.ylim([0,1])
+    plt.legend(bbox_to_anchor=(.4,.4),bbox_transform=plt.gcf().transFigure)
+    # Shrink current axis by 20%
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+
+    # Put a legend to the right of the current axis
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.savefig('figs/'+target_folder+'_results.png')
+    plt.show()
+
 target_folder = "test_full"
-
-'''
-print len(sys.argv)
-for arg in sys.argv:
-    print arg
-'''
-
+if len(sys.argv)>1:
+    target_folder = sys.argv[1]
 outfolder = 'out/'+target_folder
-a=np.fromfile( op.join(outfolder , 'linear.dat') )
-print a
-linear=a.reshape(len(a)/2,2)
-plt.plot( linear[:,0], linear[:,1], 'ro-', label='linear')
-plt.hold(True)
 
-a=np.fromfile( op.join(outfolder, 'square.dat') )
-linear=a.reshape(len(a)/2,2)
-plt.plot( linear[:,0], linear[:,1], 'bo-', label='square')
+try:
+    fig = plt.figure()
+    ax = plt.subplot(111)
+    a=np.fromfile( op.join(outfolder , 'linear.dat') )
+    print a
+    linear=a.reshape(len(a)/2,2)
+    plt.plot( linear[:,0], linear[:,1], 'ro-', label='linear') #ms=10
+    plt.hold(True)
+    a=np.fromfile( op.join(outfolder, 'square.dat') )
+    linear=a.reshape(len(a)/2,2)
+    plt.plot( linear[:,0], linear[:,1], 'bo-', label='square')
+    a=np.fromfile( op.join(outfolder, 'felzenszwalb.dat') )
+    linear=a.reshape(len(a)/2,2)
+    plt.plot( linear[:,0], linear[:,1], 'ko-', label='felzenszwalb')
+    a=np.fromfile( op.join(outfolder, 'threshold.dat') )
+    linear=a.reshape(len(a)/2,2)
+    plt.plot( linear[:,0], linear[:,1], 'go-', label='threshold')
+    formatAndSave(ax)
+except:
+    formatAndSave(ax)
 
-a=np.fromfile( op.join(outfolder, 'felzenszwalb.dat') )
-linear=a.reshape(len(a)/2,2)
-plt.plot( linear[:,0], linear[:,1], 'ko-', label='felzenszwalb')
-
-
-a=np.fromfile( op.join(outfolder, 'threshold.dat') )
-linear=a.reshape(len(a)/2,2)
-plt.plot( linear[:,0], linear[:,1], 'go-', label='threshold')
-
-
-plt.xlabel('Rand Split')
-plt.ylabel('Rand Merge')
-plt.legend(bbox_to_anchor=(.5,.5),bbox_transform=plt.gcf().transFigure)
-plt.savefig('figs/'+target_folder+'_results.png')
-plt.show()
 
---
+++ py/evaluate_c.py
@@ -0,0 +1,57 @@
+import numpy as np
+import sys
+import array
+import time
+import os
+import h5py
+sys.path.append('src_cython')
+from mainDefs import eval
+from multiprocessing import Pool
+start = time.clock()
+p=0
+
+def evaluateFile(hdf5_gt_file,hdf5_pred_file,threshes,out):
+    hdf5_gt = h5py.File(hdf5_gt_file, 'r')
+    hdf5_aff = h5py.File(hdf5_aff_file, 'r')
+    gt = np.asarray(hdf5_gt[hdf5_gt.keys()[0]],dtype='uint32')
+    aff = np.asarray(hdf5_aff[hdf5_aff.keys()[0]],dtype='float32')
+    aff = aff.transpose(3,2,1,0)
+    dims = np.array(aff.shape,dtype='uint32')
+    print 'dims:',aff.shape
+
+    # trim gt data - only works for perfect cubes
+    gt_data_dimension = gt.shape[0]
+    data_dimension = aff.shape[1]
+    if gt_data_dimension != data_dimension:
+        print("Data dimension do not match. Clip the GT borders.")
+        padding = (gt_data_dimension - data_dimension) / 2
+        gt = gt[padding:(-1*padding),padding:(-1*padding),padding:(-1*padding)]
+        print"New GT data shape :",gt.shape
+
+    # evaluate call
+    print "gt shape:",gt.shape
+    print "aff shape:",aff.shape
+    eval(gt,aff,threshes,out)
+
+    end = time.clock()
+    print "time elapsed ",end-start
+
+########################### FIBSEM ######################################
+hdf5_gt_file = '/groups/turaga/home/turagas/data/FlyEM/fibsem_medulla_7col/tstvol-520-1-h5/groundtruth_seg_thick.h5'
+hdf5_aff_file = '/groups/turaga/home/turagas/data/FlyEM/fibsem_medulla_7col/tstvol-520-1-h5/groundtruth_aff.h5'
+hdf5_pred_file = '/tier2/turaga/turagas/research/pygt_models/fibsem5/test_out_0.h5' #'',0,2,3,4,6
+
+'''
+global p
+p = Pool(numWorkers)
+print "Parallel Pool:",numWorkers
+'''
+
+# threshes
+threshes = [100+i*100 for i in range(0,10)]+[i*1000 for i in range(2,11)]+[i*10000 for i in range(2,11)] # 100...1,000...100,000
+print threshes
+
+# output folder
+out = 'out/null/'
+
+evaluateFile(hdf5_gt_file,hdf5_pred_file,threshes,out)
