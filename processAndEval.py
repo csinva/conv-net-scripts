ab34f8a26face6ba1e7de3bb30915230a29c2d21
csinva <csinva@yahoo.com>
2016-01-25 21:00:43 -0500

    removed everything but square

--- processAndEval.py
+++ processAndEval.py
@@ -7,14 +7,14 @@ from evaluateFile import evaluateFile, averageFiles
 from processFile import processFile
 
 # gt file
-train = True # which dataset to evaluate
+train = False # which dataset to evaluate
 t = "train" if train else "test"
 vol = "1" if train else "2"
 
 # input models
 model_base_folder = '/groups/turaga/home/turagas/research/caffe_v1/pygt_models/fibsem'
 fibsemFolders = ['2','3','4','5','6']
-iters = [10000*i for i in range(1,21)]
+iters = [10000*i for i in range(14,21)]
 hdf5_gt_file = '/groups/turaga/home/turagas/data/FlyEM/fibsem_medulla_7col/tstvol-520-'+vol+'-h5/groundtruth_seg_thick.h5' #groundtruth_aff.h5
 
 # settings
@@ -31,12 +31,11 @@ randOutputFolder = ['data_tier2/'+t+'/out/fibsem' +fibsemFolders[i]+ '_'+str(ite
 
 for iter_idx in range(len(iters)):
 	for fibsem_idx in range(len(fibsemFolders)):
-		'''
+		
 		if process:
 			processFile(model_base_folder+fibsemFolders[fibsem_idx]+'/',iters[iter_idx],h5OutputFilenames[iter_idx*len(fibsemFolders)+fibsem_idx],train)
 		if eval:
 			evaluateFile([hdf5_gt_file,h5OutputFilenames[iter_idx*len(fibsemFolders)+fibsem_idx]+'.h5',threshes,funcs,save_threshes,randOutputFolder[iter_idx*len(fibsemFolders)+fibsem_idx]])
-		'''
 	# this part might not work
 	h5_filenames_to_average = ["/tier2/turaga/singhc/"+t+"/output_"+str(iters[iter_idx])+"/"+"tstvol-"+vol+"_"+fibsemFolders[i] for i in range(len(fibsemFolders))]
 	out_folder = '/tier2/turaga/singhc/'+t+'/out/fibsemave_'+str(iters[iter_idx])
