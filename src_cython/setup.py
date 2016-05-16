72365491083783105e29a7225a96e4a5e517df24
Chandan <cs3hq@virginia.edu>
2016-05-16 13:02:37 -0400

    setup for working on mac

--- src_cython/setup.py
+++ src_cython/setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
-
+import numpy
 ext_modules = [Extension("zwatershed",
                          ["zwatershed.pyx", "zwatershed_main.cpp"],
                          library_dirs = ["."],
-                         include_dirs = ["."],
+                         include_dirs = [".",numpy.get_include()],
                          language='c++',
                          # std= 'c++11',
                          extra_link_args=["-std=c++11"],
--- src_cython/zwatershed.pyx
+++ src_cython/zwatershed.pyx
@@ -3,16 +3,18 @@ from libcpp.map cimport map
 from libcpp.vector cimport vector
 from libcpp.string cimport string
 from libcpp.pair cimport pair
+from libc.stdint cimport uint32_t
 import numpy as np
 import os
 cimport numpy as np
 import h5py
 
 cdef extern from "zwatershed.h":
-    map[string,vector[double]] eval_c(int dx, int dy, int dz, int dcons, np.uint32_t* gt, np.float32_t* affs, list[int] *threshes, list[string] *funcs, list[int] *save_threshes, string* out)
+    # map[string,vector[double]] eval_c(int dx, int dy, int dz, int dcons, np.uint32_t* gt, np.float32_t* affs, list[int] *threshes, list[string] *funcs, list[int] *save_threshes, string* out)
     map[string,vector[double]] oneThresh(int dx, int dy, int dz, int dcons, np.uint32_t* gt, np.float32_t* affs, int thresh, int evaluate)
     map[string,vector[double]] oneThresh_no_gt(int dx, int dy, int dz, int dcons, np.float32_t* affs, int thresh, int evaluate)
 
+'''
 def eval(np.ndarray[np.uint32_t,ndim=3] gt,np.ndarray[np.float32_t,ndim=4] affs, list[int] threshes, list[string] funcs, list[int] save_threshes, string out='out/'):
     dims = affs.shape
     dirs = [out,out+'square']
@@ -21,8 +23,9 @@ def eval(np.ndarray[np.uint32_t,ndim=3] gt,np.ndarray[np.float32_t,ndim=4] affs,
             os.makedirs(dirs[i])
     map = eval_c(dims[0],dims[1],dims[2],dims[3],&gt[0,0,0],&affs[0,0,0,0],&threshes,&funcs,&save_threshes,&out)
     return map
+'''
 
-def evalAll(np.ndarray[np.uint32_t,ndim=3] gt,np.ndarray[np.float32_t,ndim=4] affs, threshes, save_threshes,int eval, int h5, seg_save_path="NULL/"):
+def evalAll(np.ndarray[uint32_t,ndim=3] gt,np.ndarray[np.float32_t,ndim=4] affs, threshes, save_threshes,int eval, int h5, seg_save_path="NULL/"):
     if not seg_save_path.endswith("/"):
         seg_save_path = seg_save_path + "/"
         if not os.path.exists(seg_save_path):
@@ -82,7 +85,7 @@ def watershedAll_no_eval(np.ndarray[np.float32_t,ndim=4] affs, threshes, save_th
                 f.close()
     if not h5==1:
         return segs
-    
+
 
 def zwatershed_and_metrics(gt,affs,threshes,save_threshes):
     return evalAll(gt,affs,threshes,save_threshes,eval=1,h5=0)
