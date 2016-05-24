b8772c0eda26fcb64a20befce6e06bc7b055f877
Chandan <cs3hq@virginia.edu>
2016-05-24 13:44:53 -0400

    passing in proper seg

--- arb_nhoods/test_eval.py
+++ arb_nhoods/test_eval.py
@@ -15,14 +15,13 @@ import matplotlib.pyplot as plt
 import array
 import matplotlib
 
-sys.path.append('arb_nhoods')
-import twatershed as tw
-
 sys.path.append('../')
+import twatershed as tw
 from visualization.visualize_funcs import *
 
 path_to_folder = '/Users/chandansingh/drive/janelia/conv_net_scripts/'
 path_to_data = path_to_folder + 'data/'
+
 
 threshes = [500, 1000, 2000, 10000, 20000, 1e6,
             1e9]  # for i in range(1,6)]+[i*20000 for i in range(2,16)] # 100...1,000...100,000
@@ -42,8 +41,6 @@ aff = np.asarray(hdf5_aff[hdf5_aff.keys()[0]], dtype='float32')
 aff = aff[:, p_small:(-1 * p_small), p_small:(-1 * p_small), p_small:(-1 * p_small)]
 
 dims = np.array(aff.shape, dtype='uint32')
-print('dims:', aff.shape)
-
 # trim gt data - only works for perfect cubes
 gt_data_dimension = gt.shape[0]
 data_dimension = aff.shape[1]
@@ -57,20 +54,18 @@ nhood = tw.mknhood3d(1)
 node1, node2, edge_affs = tw.affgraph_to_edgelist(aff, nhood)
 node1 = np.array(node1, dtype='int32')
 node2 = np.array(node2, dtype='int32')
-THRESH = .9999  # higher is more connected
+THRESH = .999  # higher is more connected
 # print node1[0:40]
 # print node2[0:40]
-edge_affs_thresh = np.array(edge_affs <= THRESH, dtype='int32')
+edge_affs_thresh = 1-np.array(edge_affs <= THRESH, dtype='int32')
 print "edge_affs: ", edge_affs_thresh
 print "percent below thresh", sum(edge_affs <= THRESH) / float(len(edge_affs))
 seg_cc, _ = tw.connected_components(int(np.size(gt)), node1, node2, edge_affs_thresh)
 # seg_cc = gt
-print "num segs seg_cc", max(seg_cc.flatten())
-print "num segs gt", max(gt.flatten())
+# seg_cc = np.zeros(gt.shape,dtype='int32')
+print "num segs gt,seg", max(gt.flatten()),max(seg_cc.flatten())
 seg, seg_sizes = tw.marker_watershed(seg_cc.flatten(), node1, node2, edge_affs, threshes)
-# print "output_seg_len", len(seg_sizes)
-print "num segs seg_cc", max(seg.flatten())
-print "num segs gt", max(gt.flatten())
+print "num segs gt,seg", max(gt.flatten()),max(seg.flatten())
 seg = seg.reshape(gt.shape)
 
 aff = aff.transpose(1, 2, 3, 0)
