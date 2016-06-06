719c30ad42944339550213616608ec6f8521c71e
Chandan <cs3hq@virginia.edu>
2016-06-06 12:19:58 -0400

    only keep minimal in original loop

--- src_cython/zwatershed_util/basic_watershed_arb.hpp
+++ src_cython/zwatershed_util/basic_watershed_arb.hpp
@@ -29,7 +29,7 @@ watershed_arb(int xdim, int ydim, int zdim, ID* node1, ID* node2, F* edgeWeight,
     id_t* seg_raw = seg.data();
     for(ID i=0;i<size;i++)
         seg_raw[i]=traits::high_bit;
-    //high_bit=1 means background, 2nd high_bit=1 means visited
+    //high_bit=1 means background, 2nd high_bit=1 means visited, val stores min_index
 
     // 1 - filter by Tmax, Tmin
     map<ID,F> maxes; //find maxes for each vertex
@@ -40,8 +40,8 @@ watershed_arb(int xdim, int ydim, int zdim, ID* node1, ID* node2, F* edgeWeight,
         ID v2 = node2[i];
         if(aff>=low){ //1b For each {u, v} from E set w({vi, u}) = 0 if w({vi, u}) < Tmin.
             weights[make_pair(v1,v2)] = aff;
-            seg_raw[node1[i]] &= traits::mask; //1c set all vertices with no edges as background
-            seg_raw[node2[i]] &= traits::mask;
+            seg_raw[v1] &= traits::mask; //1c set all vertices with no edges as background
+            seg_raw[v2] &= traits::mask;
             if(!maxes.count(v1))
                 maxes[v1] = aff;
             else if(aff > maxes[v1])
@@ -54,42 +54,50 @@ watershed_arb(int xdim, int ydim, int zdim, ID* node1, ID* node2, F* edgeWeight,
     }
 
     // 2 - keep only outgoing edges with max edge (can be multiple)
+    // 3 keep only one strictly outgoing edge pointing to a vertex with the minimal index
     map<ID, unordered_set<ID> > edges;
+    queue<ID> vqueue;
     for ( const auto &pair : weights ) {
         ID v1 = pair.first.first;
         ID v2 = pair.first.second;
         F aff = pair.second;
-        if(aff==maxes[v1] || aff >=high)
-            edges[v1].insert(v2);
-        if(aff==maxes[v2] || aff >=high) // not bidirectional
-            edges[v2].insert(v1);
+        if(aff==maxes[v1] || aff >=high){
+            if(aff==maxes[v2] || aff >=high){ // bidirectional
+                edges[v1].insert(v2);
+                edges[v2].insert(v1);
+            }
+            else{                             // not bidirectional
+                if(!(seg_raw[v1]&traits::mask_3)){
+                    seg_raw[v1]+=v2;
+                    edges[v1].insert(v2);
+                }
+                else if(v2 < seg_raw[v1]&traits::mask_3){
+                    edges[v1].erase(seg_raw[v1]&traits::mask_3); // this might cause a problem
+                    seg_raw[v1] = seg_raw[v1]&&traits::mask_high + v2;
+                    edges[v1].insert(v2);
+                }
+            }
+        }
+        else if(aff==maxes[v2] || aff >=high){ // not bidirectional
+            if(!(seg_raw[v2]&traits::mask_3)){
+                seg_raw[v2]+=v1;
+                edges[v2].insert(v1);
+            }
+            else if(v1 < seg_raw[v2]&traits::mask_3){
+                edges[v2].erase(seg_raw[v2]&traits::mask_3); // this might cause a problem
+                seg_raw[v2] = seg_raw[v2]&&traits::mask_high + v1;
+                edges[v2].insert(v1);
+            }
+
+        }
     }
     weights.clear();
 
-    // 3 keep only one strictly outgoing edge pointing to a vertex with the minimal index
-    map<ID,ID> min_indexes;
-    queue<ID> vqueue;
+
     for ( const auto &pair : edges ) {
         for( const auto &v2:pair.second){
             ID v1 = pair.first;
             if(edges[v2].find(v1)==edges[v2].end()){ // if strictly outgoing
-                /*
-                if(!(seg_raw[v1]&traits::mask_3))
-                    seg_raw[v1]+=v2;
-                else if(v2 < seg_raw[v1]&traits::mask_3){
-                    edges[v1].erase(min_indexes[v1]); // this might cause a problem
-                    seg_raw[v1] = seg_raw[v1]&&traits::mask_high + v2;
-                }
-                */
-
-                if(!min_indexes.count(v1))
-                    min_indexes[v1] = v2;
-                else if(v2 < min_indexes[v1]){
-                    edges[v1].erase(min_indexes[v1]);
-                    min_indexes[v1]=v2;
-                }
-
-
                 if(!(seg_raw[v1]&traits::high_bit_2)){
                     vqueue.push(v1);
                     seg_raw[v1]|=traits::high_bit_2;                    //visited=true
