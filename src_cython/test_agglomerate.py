7552ea5995ae0678b1535eebad1ba07a9b4a5c73
csinva <csinva@yahoo.com>
2016-06-22 18:42:16 -0400

    test agglomerate

---
+++ src_cython/test_agglomerate.py
@@ -0,0 +1,80 @@
+import numpy as np
+import sys
+import time
+import os
+import h5py
+import os.path as op
+import matplotlib
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+from multiprocessing import Pool
+from itertools import product
+from par_funcs import *
+sys.path.append('..')
+V = 20
+
+# -------------------------------- parameters ---------------------------------------
+# pred_file = '/groups/turaga/home/turagas/research/caffe_v2/processed/bock2/120000/sample_A_x1_y1_z1_xy1.h5'
+pred_file = '/groups/turaga/home/turagas/research/caffe_v2/processed/bock2/120000/cutout_3k.h5'
+# pred_file = '/groups/turaga/home/turagas/turagalab/FROM_TIER2/singhc/train/output_200000/tstvol-1_2.h5'
+out_folder = '/nobackup/turaga/singhc/3k_250/' # _3k _vol has full, max_len = 300
+outname = out_folder+'out.h5'
+NUM_WORKERS = 32
+MAX_LEN = 250
+
+
+
+
+
+def merge_by_thresh(seg,seg_sizes,rg,thresh):
+    re = {}
+    seg_max = np.max(seg)
+    seg_min = np.min(seg)
+    print "calculating renums..."
+    t1 =time.time()
+    for i in range(rg.shape[0]):
+        n1,n2,w = rg[i,:]
+        size = w*w*thresh
+        if seg_sizes[n1] < size or seg_sizes[n2] < size:
+            re[n2]=n1
+            seg_sizes[n1]+=seg_sizes[n2]
+            seg_sizes[n2]+=seg_sizes[n1]
+    re_filtered = {}
+    print "calc renums time",time.time()-t1,"secs"
+    t1 =time.time()
+    print "filtering renums..."
+    for key in re:
+        val = re[key]
+        while val in re:
+            val = re[val]
+        if key < seg_max and val < seg_max:
+            re_filtered[key] = val
+    print "filtering renums time",time.time()-t1,"secs"
+    t1 =time.time()
+    print "renumbering..."
+    mp = np.arange(0,seg_max+1,dtype='uint64')
+    mp[re_filtered.keys()] = re_filtered.values()
+    seg = mp[seg]
+    print "renumbering time",time.time()-t1,"secs"
+    t1 =time.time()
+    return seg
+
+
+
+
+num,thresh = 0,2000
+f = h5py.File(outname, 'a')
+s,e = f['starts'][num],f['ends'][num]
+seg = f['seg'][s[0]:e[0]-3,s[1]:e[1]-3,s[2]:e[2]-3]
+seg_sizes = np.array(f['seg_sizes'])
+rg = np.array(f['rg_'+str(num)])
+f.close()
+print "num_segs",len(np.unique(seg_init)),len(np.unique(seg))
+print "rg lens",len(rg_init),len(rg)
+
+
+seg_merged = merge_by_thresh(seg,seg_sizes,rg,thresh)
+
+print "num_segs",len(np.unique(seg_init)),len(np.unique(seg))
+print "rg lens",len(rg_init),len(rg)
+
--- src_cython/test_stitch.py
+++ src_cython/test_stitch.py
@@ -17,10 +17,10 @@ V = 20
 # pred_file = '/groups/turaga/home/turagas/research/caffe_v2/processed/bock2/120000/sample_A_x1_y1_z1_xy1.h5'
 pred_file = '/groups/turaga/home/turagas/research/caffe_v2/processed/bock2/120000/cutout_3k.h5'
 # pred_file = '/groups/turaga/home/turagas/turagalab/FROM_TIER2/singhc/train/output_200000/tstvol-1_2.h5'
-out_folder = '/nobackup/turaga/singhc/3k_250/' # _3k _vol has full, max_len = 300
+out_folder = '/nobackup/turaga/singhc/3k_200/' # _3k _vol has full, max_len = 300
 outname = out_folder+'out.h5'
 NUM_WORKERS = 32
-MAX_LEN = 250
+MAX_LEN = 200
 
 t1 = time.time()
 partition_data = partition_subvols(pred_file,out_folder,max_len=MAX_LEN)
@@ -28,8 +28,8 @@ print "time",time.time()-t1,"secs"
 
 def stitch_and_save(partition_data,outname):
     args,starts,ends,dims,num_vols = partition_data
-#     (X,Y,Z) = num_vols #(1,1,2) # num_vols
-    X,Y,Z = 1,1,2
+    (X,Y,Z) = num_vols #(1,1,2) # num_vols
+    # X,Y,Z = 1,1,2
     if not outname.endswith('.h5'):
         outname += '.h5'
     if op.isfile(outname):
@@ -43,11 +43,11 @@ def stitch_and_save(partition_data,outname):
 
     print "create dataset time",time.time()-t1,"secs"
     t1 = time.time()
-
+    t2 = time.time()
     # calc all merges, set dset_seg, rg with incrementing
     for x,y,z in product(range(X),range(Y),range(Z)):
         i = x*num_vols[1]*num_vols[2]+y*num_vols[2]+z
-        print "i =",str(i-1),"time",time.time()-t1,"secs"
+        print "\ti =",str(i-1),"time",time.time()-t1,"secs"
         t1 = time.time()
         i_arr.append(i)
         s,e = starts[i],ends[i]
@@ -57,7 +57,7 @@ def stitch_and_save(partition_data,outname):
         rg[:,:2] += inc
         rgs[i] = rg
         inc = np.max(seg)
-        print "i,x,y,z",i,x,y,z
+        print "\ti,x,y,z",i,x,y,z
         if not z==0: 
             re,merges = calc_merges(edge_mins=dset_seg[s[0]:e[0],s[1]:e[1],s[2]+3],edge_maxes=seg[:,:,3], re=re, merges=merges)
         if not y==0:
@@ -67,6 +67,7 @@ def stitch_and_save(partition_data,outname):
         dset_seg[s[0]:e[0],s[1]:e[1],s[2]:e[2]] = seg[:,:,:]
 #         plt.imshow(dset_seg[0, :, :], cmap=cmap)
 #         plt.show()
+    print "calculate all merges",time.time()-t2,"secs"
 
     t1 = time.time()
     merges_filtered = filter_merges(merges)
